e3d6a6c5e9be223b5d1188b88b05f091c86b5086
Sergej Jaudszims <redtape9688@gmail.com>
2024-10-10 23:15:09 +0200

    update actual job info

--- lib/data.ts
+++ lib/data.ts
@@ -39,7 +39,7 @@ export const experiencesData = [
     location: "Adesso Insurance Solutions GmbH - Hamburg",
     description:
       "Als Java-Entwickler optimiere ich den reibungslosen Zahlungsverkehr im Versicherungswesen, indem ich unsere leistungsstarke Standardsoftware PayTras kontinuierlich weiterentwickle und präzise auf die individuellen Anforderungen unserer Kunden zuschneide.",
-    icon: React.createElement(PiCurrencyCircleDollar),
+    icon: React.createElement(FaLaptopCode),
     date: "07/2024 - heute",
   },
   {
